420e4d9b5241129f4016fa63d14191c5278b8a93
galaxy <549278303@qq.com>
2016-07-16 21:05:31 +0800

    test

--- test.php
+++ test.php
@@ -4,11 +4,7 @@ include 'model.php';
 $sql=new SQL();
 $sql->table='test';
 $sql->type='s';
-echo $sql->add('col_1','1');
-echo "\n";
-echo $sql->get_num();
-echo "\n";
-print_r($sql->get_case());
+echo $sql->generate();
 
 
 ?>
