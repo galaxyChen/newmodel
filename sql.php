55674d74f9d2ce86f415147b8488affd35e1ec10
galaxy <549278303@qq.com>
2016-07-16 20:24:02 +0800

    fix name conflict

--- sql.php
+++ sql.php
@@ -70,8 +70,8 @@ class SQL
 		if ($this->query_num==0) return $sql;
 		$sql.=' WHERE ';
 		for ($i=0;$i<$this->query_num-1;$i++)
-			$sql.=$this->case[$i].'='.real_values($this->values[$i]).' AND ';
-		$sql.=$this->case[$this->query_num-1].'='.real_values($this->values[$this->query_num-1]).';';
+			$sql.=$this->case[$i].'='.real_value($this->values[$i]).' AND ';
+		$sql.=$this->case[$this->query_num-1].'='.real_value($this->values[$this->query_num-1]).';';
 		return $sql;
 	}
 }
