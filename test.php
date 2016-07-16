1702a3cc0e8406f49d55c0a0f29aa9506f6f80c9
galaxy <549278303@qq.com>
2016-07-16 21:38:51 +0800

    test number

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include 'model.php';
 $sql=new SQL();
 $sql->table='test';
 $sql->type='s';
-$sql->add('col_1','17546534543');
+$sql->add('col_1','175465345431111111111111');
 echo $sql->generate();
 print_r($sql->get_values());
 
