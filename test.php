1b05b026dfb02404433b02bdd1f1edc4d4988eaa
galaxy <549278303@qq.com>
2016-07-18 11:25:12 +0800

    new test

--- test.php
+++ test.php
@@ -5,9 +5,5 @@ $sql=new SQL();
 $sql->table='test';
 $sql->type='s';
 $sql->add('col_1','1');
-$sql->add_col('col_1');
-echo $sql->generate();
-print_r($sql->get_values());
-
-
+print_r(mysqli_select($sql));
 ?>
